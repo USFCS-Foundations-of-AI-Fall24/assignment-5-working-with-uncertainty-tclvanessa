fcff2308540f9f7027f556272c7f628291460347
Vanessa Le <vcle@dons.usfca.edu>
2024-11-11 00:10:48 -0800

    alarm.py finished and queries for carnet.py inputted

--- alarm.py
+++ alarm.py
@@ -1,61 +1,76 @@
 from pgmpy.models import BayesianNetwork
 from pgmpy.inference import VariableElimination
-
-alarm_model = BayesianNetwork(
-    [
-        ("Burglary", "Alarm"),
-        ("Earthquake", "Alarm"),
-        ("Alarm", "JohnCalls"),
-        ("Alarm", "MaryCalls"),
-    ]
-)
-
-# Defining the parameters using CPT
 from pgmpy.factors.discrete import TabularCPD
 
-cpd_burglary = TabularCPD(
-    variable="Burglary", variable_card=2, values=[[0.999], [0.001]],
-    state_names={"Burglary":['no','yes']},
-)
-cpd_earthquake = TabularCPD(
-    variable="Earthquake", variable_card=2, values=[[0.998], [0.002]],
-    state_names={"Earthquake":["no","yes"]},
-)
-cpd_alarm = TabularCPD(
-    variable="Alarm",
-    variable_card=2,
-    values=[[0.999, 0.71, 0.06, 0.05], [0.001, 0.29, 0.94, 0.95]],
-    evidence=["Burglary", "Earthquake"],
-    evidence_card=[2, 2],
-    state_names={"Burglary":['no','yes'], "Earthquake":['no','yes'], 'Alarm':['yes','no']},
-)
-cpd_johncalls = TabularCPD(
-    variable="JohnCalls",
-    variable_card=2,
-    values=[[0.95, 0.1], [0.05, 0.9]],
-    evidence=["Alarm"],
-    evidence_card=[2],
-    state_names={"Alarm":['yes','no'], "JohnCalls":['yes', 'no']},
-)
-cpd_marycalls = TabularCPD(
-    variable="MaryCalls",
-    variable_card=2,
-    values=[[0.1, 0.7], [0.9, 0.3]],
-    evidence=["Alarm"],
-    evidence_card=[2],
-state_names={"Alarm":['yes','no'], "MaryCalls":['yes', 'no']},
-)
+def create_alarm_model():
+    alarm_model = BayesianNetwork(
+        [
+            ("Burglary", "Alarm"),
+            ("Earthquake", "Alarm"),
+            ("Alarm", "JohnCalls"),
+            ("Alarm", "MaryCalls"),
+        ]
+    )
 
-# Associating the parameters with the model structure
-alarm_model.add_cpds(
-    cpd_burglary, cpd_earthquake, cpd_alarm, cpd_johncalls, cpd_marycalls)
+    # Defining the parameters using CPT
+    cpd_burglary = TabularCPD(
+        variable="Burglary", variable_card=2, values=[[0.999], [0.001]],
+        state_names={"Burglary": ['no', 'yes']},
+    )
+    cpd_earthquake = TabularCPD(
+        variable="Earthquake", variable_card=2, values=[[0.998], [0.002]],
+        state_names={"Earthquake": ["no", "yes"]},
+    )
+    cpd_alarm = TabularCPD(
+        variable="Alarm",
+        variable_card=2,
+        values=[[0.999, 0.71, 0.06, 0.05], [0.001, 0.29, 0.94, 0.95]],
+        evidence=["Burglary", "Earthquake"],
+        evidence_card=[2, 2],
+        state_names={"Burglary": ['no', 'yes'], "Earthquake": ['no', 'yes'], 'Alarm': ['yes', 'no']},
+    )
+    cpd_johncalls = TabularCPD(
+        variable="JohnCalls",
+        variable_card=2,
+        values=[[0.95, 0.1], [0.05, 0.9]],
+        evidence=["Alarm"],
+        evidence_card=[2],
+        state_names={"Alarm": ['yes', 'no'], "JohnCalls": ['yes', 'no']},
+    )
+    cpd_marycalls = TabularCPD(
+        variable="MaryCalls",
+        variable_card=2,
+        values=[[0.1, 0.7], [0.9, 0.3]],
+        evidence=["Alarm"],
+        evidence_card=[2],
+        state_names={"Alarm": ['yes', 'no'], "MaryCalls": ['yes', 'no']},
+    )
 
-alarm_infer = VariableElimination(alarm_model)
+    # Associating the parameters with the model structure
+    alarm_model.add_cpds(cpd_burglary, cpd_earthquake, cpd_alarm, cpd_johncalls, cpd_marycalls)
+    return alarm_model
 
-print(alarm_infer.query(variables=["JohnCalls"],evidence={"Earthquake":"yes"}))
-# q = alarm_infer.query(variables=["JohnCalls", "Earthquake"],evidence={"Burglary":"yes","MaryCalls":"yes"})
-# print(q)
+if __name__ == "__main__":
+    alarm_model = create_alarm_model()
+    alarm_infer = VariableElimination(alarm_model)
 
-# the probability of Mary Calling given that John called
-q = alarm_infer.query(variables=["MaryCalls"],evidence={"JohnCalls":"yes"})
-print(q)
+    # Original query: Probability of John calling given an earthquake
+    print("Probability of JohnCalls given Earthquake:")
+    print(alarm_infer.query(variables=["JohnCalls"], evidence={"Earthquake": "yes"}))
+    q = alarm_infer.query(variables=["JohnCalls", "Earthquake"], evidence={"Burglary": "yes", "MaryCalls": "yes"})
+    print(q)
+
+    # Probability of Mary calling given that John called
+    print("\nProbability of MaryCalls given JohnCalls:")
+    q = alarm_infer.query(variables=["MaryCalls"], evidence={"JohnCalls": "yes"})
+    print(q)
+
+    # Probability of both John and Mary calling given Alarm
+    print("\nProbability of both JohnCalls and MaryCalls given Alarm:")
+    q = alarm_infer.query(variables=["MaryCalls", "JohnCalls"], evidence={"Alarm": "yes"})
+    print(q)
+
+    # Probability of Alarm, given that Mary called
+    print("\nProbability of Alarm given MaryCalls and JohnCalls:")
+    q = alarm_infer.query(variables=["Alarm"], evidence={"JohnCalls": "yes", "MaryCalls": "yes"})
+    print(q)
--- carnet.py
+++ carnet.py
@@ -1,71 +1,94 @@
 from pgmpy.models import BayesianNetwork
 from pgmpy.inference import VariableElimination
 
-car_model = BayesianNetwork(
-    [
-        ("Battery", "Radio"),
-        ("Battery", "Ignition"),
-        ("Ignition","Starts"),
-        ("Gas","Starts"),
-        ("Starts","Moves")
-    ]
-)
+def create_car_model():
+    car_model = BayesianNetwork(
+        [
+            ("Battery", "Radio"),
+            ("Battery", "Ignition"),
+            ("Ignition","Starts"),
+            ("Gas","Starts"),
+            ("Starts","Moves")
+        ]
+    )
 
-# Defining the parameters using CPT
-from pgmpy.factors.discrete import TabularCPD
+    # Defining the parameters using CPT
+    from pgmpy.factors.discrete import TabularCPD
 
-cpd_battery = TabularCPD(
-    variable="Battery", variable_card=2, values=[[0.70], [0.30]],
-    state_names={"Battery":['Works',"Doesn't work"]},
-)
+    cpd_battery = TabularCPD(
+        variable="Battery", variable_card=2, values=[[0.70], [0.30]],
+        state_names={"Battery":['Works',"Doesn't work"]},
+    )
 
-cpd_gas = TabularCPD(
-    variable="Gas", variable_card=2, values=[[0.40], [0.60]],
-    state_names={"Gas":['Full',"Empty"]},
-)
+    cpd_gas = TabularCPD(
+        variable="Gas", variable_card=2, values=[[0.40], [0.60]],
+        state_names={"Gas":['Full',"Empty"]},
+    )
 
-cpd_radio = TabularCPD(
-    variable=  "Radio", variable_card=2,
-    values=[[0.75, 0.01],[0.25, 0.99]],
-    evidence=["Battery"],
-    evidence_card=[2],
-    state_names={"Radio": ["turns on", "Doesn't turn on"],
-                 "Battery": ['Works',"Doesn't work"]}
-)
+    cpd_radio = TabularCPD(
+        variable=  "Radio", variable_card=2,
+        values=[[0.75, 0.01],[0.25, 0.99]],
+        evidence=["Battery"],
+        evidence_card=[2],
+        state_names={"Radio": ["turns on", "Doesn't turn on"],
+                     "Battery": ['Works',"Doesn't work"]}
+    )
 
-cpd_ignition = TabularCPD(
-    variable=  "Ignition", variable_card=2,
-    values=[[0.75, 0.01],[0.25, 0.99]],
-    evidence=["Battery"],
-    evidence_card=[2],
-    state_names={"Ignition": ["Works", "Doesn't work"],
-                 "Battery": ['Works',"Doesn't work"]}
-)
+    cpd_ignition = TabularCPD(
+        variable=  "Ignition", variable_card=2,
+        values=[[0.75, 0.01],[0.25, 0.99]],
+        evidence=["Battery"],
+        evidence_card=[2],
+        state_names={"Ignition": ["Works", "Doesn't work"],
+                     "Battery": ['Works',"Doesn't work"]}
+    )
 
-cpd_starts = TabularCPD(
-    variable="Starts",
-    variable_card=2,
-    values=[[0.95, 0.05, 0.05, 0.001], [0.05, 0.95, 0.95, 0.9999]],
-    evidence=["Ignition", "Gas"],
-    evidence_card=[2, 2],
-    state_names={"Starts":['yes','no'], "Ignition":["Works", "Doesn't work"], "Gas":['Full',"Empty"]},
-)
+    cpd_starts = TabularCPD(
+        variable="Starts",
+        variable_card=2,
+        values=[[0.95, 0.05, 0.05, 0.001], [0.05, 0.95, 0.95, 0.9999]],
+        evidence=["Ignition", "Gas"],
+        evidence_card=[2, 2],
+        state_names={"Starts":['yes','no'], "Ignition":["Works", "Doesn't work"], "Gas":['Full',"Empty"]},
+    )
 
-cpd_moves = TabularCPD(
-    variable="Moves", variable_card=2,
-    values=[[0.8, 0.01],[0.2, 0.99]],
-    evidence=["Starts"],
-    evidence_card=[2],
-    state_names={"Moves": ["yes", "no"],
-                 "Starts": ['yes', 'no'] }
-)
+    cpd_moves = TabularCPD(
+        variable="Moves", variable_card=2,
+        values=[[0.8, 0.01],[0.2, 0.99]],
+        evidence=["Starts"],
+        evidence_card=[2],
+        state_names={"Moves": ["yes", "no"],
+                     "Starts": ['yes', 'no'] }
+    )
 
+    # Associating the parameters with the model structure
+    car_model.add_cpds( cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves)
+    return car_model
 
-# Associating the parameters with the model structure
-car_model.add_cpds( cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves)
+if __name__ == "__main__":
+    car_model = create_car_model()
+    car_infer = VariableElimination(car_model)
 
-car_infer = VariableElimination(car_model)
+    # Original Query: Computing the probability of the car moving given that the radio turns on and the car starts
+    print("\nProbability of the car moving given that the radio turns on and the car starts:")
+    print(car_infer.query(variables=["Moves"],evidence={"Radio":"turns on", "Starts":"yes"}))
 
-print(car_infer.query(variables=["Moves"],evidence={"Radio":"turns on", "Starts":"yes"}))
+    # Given that the car will not move, what is the probability that the battery is not working?
+    print("\nProbability that the battery is not working given that the car will not move:")
+    print(car_infer.query(variables=["Battery"],evidence={"Moves":"no"}))
 
+    # Given that the radio is not working, what is the probability that the car will not start?
+    print("\nProbability that the car will not start given that the radio is not working:")
+    print(car_infer.query(variables=["Starts"],evidence={"Radio":"Doesn't turn on"}))
 
+    # Given that the battery is working, does the probability of the radio working change if we discover that the car has gas in it?
+    print("\nProbability of the radio working given that the battery is working and that the car has gas in it:")
+    print(car_infer.query(variables=["Radio"],evidence={"Battery":"Works", "Gas":"Full"}))
+
+    # Given that the car doesn't move, how does the probability of the ignition failing change if we observe that the car dies not have gas in it?
+    print("\nProbability of the ignition failing given that the car doesn't move and that the car dies not have gas in it:")
+    print(car_infer.query(variables=["Ignition"],evidence={"Moves":"no", "Gas":"Empty"}))
+
+    # What is the probability that the car starts if the radio works and it has gas in it?
+    print("\nProbability that the car starts if the radio works and it has gas in it:")
+    print(car_infer.query(variables=["Starts"],evidence={"Radio":"turns on", "Gas":"Full"}))

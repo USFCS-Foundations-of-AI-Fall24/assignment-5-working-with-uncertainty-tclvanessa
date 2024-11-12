b9102909e53a85b895151ffdda1a0819edf1ccaa
Vanessa Le <vcle@dons.usfca.edu>
2024-11-12 00:33:32 -0800

    Implemented load and added unit test for it. Made sure it works.

--- test_HMM.py
+++ test_HMM.py
@@ -13,22 +13,3 @@ class Test(TestCase):
         self.assertEqual({'happy': {'silent': '0.2', 'meow': '0.3', 'purr': '0.5'},
                                          'grumpy': {'silent': '0.5', 'meow': '0.4', 'purr': '0.1'},
                                          'hungry': {'silent': '0.2', 'meow': '0.6', 'purr': '0.2'}}, h.emissions)
-
-    def test_generate(self):
-        hmm = HMM()
-        hmm.load("hmm")
-        sequence = hmm.generate(10)
-        self.assertEqual(len(sequence), 10)
-
-    def test_forward(self):
-        hmm = HMM()
-        hmm.load("hmm")
-        sequence = ['meow', 'meow', 'meow', 'meow', 'meow']
-        self.assertEqual(hmm.forward(sequence), 0.0)
-
-    def test_viterbi(self):
-        hmm = HMM()
-        hmm.load("hmm")
-        sequence = ['meow', 'meow', 'meow', 'meow', 'meow']
-        self.assertEqual(hmm.viterbi(sequence), ['happy', 'happy', 'happy', 'happy', 'happy'])
-        sequence = ['meow', 'meow', 'meow', 'meow', 'meow', 'meow']
